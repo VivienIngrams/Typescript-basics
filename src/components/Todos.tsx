3d125763e68a7227004abe282ca16c8e8784b29f
VivienIngrams <vivingrams@gmail.com>
2023-06-30 16:51:28 +0100

    Using context

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,12 +1,20 @@
-import Todo from "../models/todo";
+import React, { useContext } from "react";
+
 import TodoItem from "./TodoItem";
 import classes from "./Todos.module.css";
+import { TodosContext } from "../store/todos-context";
 
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC = () => {
+  const todosContext = useContext(TodosContext);
+
   return (
     <ul className={classes.todos}>
-      {props.items.map((item) => (
-        <TodoItem key={item.id} text={item.text} />
+      {todosContext.items.map((item) => (
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={todosContext.removeTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
